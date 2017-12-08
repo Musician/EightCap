fa53541a584aedd66a5516c79ed496c74c9f6661
Musician <musician@abv.bg>
2017-12-08 12:50:03 +0200

    Entities

--- test_project/src/Musician/BlogBundle/Entity/Blog.php
+++ test_project/src/Musician/BlogBundle/Entity/Blog.php
@@ -31,4 +31,82 @@ class Blog
         return $this->id;
     }
 
+     /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string")
+     * @ORM\Title
+     */
+    private $title;
+     
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+     /**
+     * @var text
+     *
+     * @ORM\Column(name="short_text", type="text")
+     * @ORM\Short_text
+     */
+    private $short_text;
+     
+    public function getShortText()
+    {
+        return $this->short_text;
+    }  
+    
+     /**
+     * @var text
+     *
+     * @ORM\Column(name="long_text", type="text")
+     * @ORM\Long_text
+     */
+    private $long_text;
+     
+    public function getLongText()
+    {
+        return $this->long_text;
+    }  
+     
+     /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string")
+     * @ORM\Author
+     */
+    private $author;
+     
+    public function getAuthor()
+    {
+        return $this->author;
+    }  
+    
+     /**
+     * @var bool
+     *
+     * @ORM\Column(name="active", type="bool")
+     * @ORM\Active
+     */
+    private $active;
+     
+    public function getActive()
+    {
+        return $this->active;
+    }  
+
+     /**
+     * @var date
+     *
+     * @ORM\Column(name="created", type="date")
+     * @ORM\Created
+     */
+    private $created;
+     
+    public function getCreated()
+    {
+        return $this->created;
+    }  
+    
 }
